9aa363658b40e95b0ad486137ba2267dfa85790b
OIITuMuCT <vataman@live.ru>
2024-09-12 21:29:02 +0300

    add customer orders

--- orders/views.py
+++ orders/views.py
@@ -23,6 +23,6 @@ def order_create(request):
     else:
         form = OrderCreateForm()
     return render(request,
-                  'orders/order/create.html',
-                  {'cart': cart, 'form': form})
+                'orders/order/create.html',
+                {'cart': cart, 'form': form})
 
