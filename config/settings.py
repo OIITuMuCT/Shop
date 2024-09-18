ca930f2d4d407b1e7a46838558e7e848205c1641
OIITuMuCT <vataman@live.ru>
2024-09-18 14:44:58 +0300

    payment succsessful

--- config/settings.py
+++ config/settings.py
@@ -40,10 +40,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'shop.apps.ShopConfig',
     'cart.apps.CartConfig',
     'orders.apps.OrdersConfig',
-    
+    'payment.apps.PaymentConfig',
+    'shop.apps.ShopConfig',
+
 ]
 
 MIDDLEWARE = [
@@ -138,8 +139,22 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CART_SESSION_ID = 'cart'
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
 
 STRIPE_PUBLISHABLE_KEY = config('STRIPE_PUBLISHABLE_KEY')
 STRIPE_SECRET_KEY = config('STRIPE_SECRET_KEY')
 STRIPE_API_VERSION = '2024-04-10'
+
+
+# Email server configuration
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
+# EMAIL_HOST = 'smtp.gmail.com'
+# EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+# EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+# EMAIL_PORT = 587
+# EMAIL_USE_TLS = True
+# DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL')
+
--- orders/views.py
+++ orders/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from .models import OrderItem
 from .forms import OrderCreateForm
 from cart.cart import Cart
@@ -11,21 +11,25 @@ def order_create(request):
         if form.is_valid():
             order = form.save()
             for item in cart:
-                OrderItem.objects.create(order=order,
-                                         product=item['product'],
-                                         price=item['price'],
-                                         quantity=item['quantity'])
+                OrderItem.objects.create(
+                    order=order,
+                    product=item['product'],
+                    price=item['price'],
+                    quantity=item['quantity'],
+                )
+            # clear the cart
             cart.clean()
             # launch asynchronous task
             order_created.delay(order.id)
-            return render(
-                request, 
-                'orders/order/created.html', 
-                {'order': order}
-            )
+            # set the order in the session
+            request.session['order_id'] = order.id
+            # redirect for payment
+            return redirect('payment:process')
     else:
         form = OrderCreateForm()
-    return render(request,
-                'orders/order/create.html',
-                {'cart': cart, 'form': form})
+    return render(
+        request,
+        'orders/order/create.html',
+        {'cart': cart, 'form': form},
+    )
 
